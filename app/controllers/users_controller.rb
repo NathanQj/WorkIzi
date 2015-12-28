13499b5f0927f22bf2366307804962fbd8c5c9fa
nath <nathan.qjm@gmail.com>
2015-12-28 22:50:06 +0100

    Fin gestion users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
   before_filter :correct_user, :only => [:edit, :update]
-
+  before_filter :admin_user,   :only => :destroy
 
   def index
     @titre = "Tous les utilisateurs"
@@ -50,6 +50,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Utilisateur supprimé."
+    redirect_to users_path
+  end
+
   private
 
   def authenticate
@@ -59,6 +65,10 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_path) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
   end
 
 end
