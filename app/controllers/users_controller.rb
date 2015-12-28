2d0603897ee5c5247f6a2798ccdef64a407b1730
nath <nathan.qjm@gmail.com>
2015-12-28 03:23:24 +0100

    Modele utilisateur avec mot de passe et profil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
+
+
   def show
     @user = User.find(params[:id])
+    @titre = @user.nom
   end
 
   def new
     @title = "S'inscrire"
   end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151227212046) do
+ActiveRecord::Schema.define(version: 20151228011956) do
 
   create_table "users", force: :cascade do |t|
     t.string   "nom"
     t.string   "email"
     t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
