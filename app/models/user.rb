8b7c16703ee596744be4ea1f2dd1a9d635133d9d
nath <nathan.qjm@gmail.com>
2015-12-28 01:26:37 +0100

    Secure

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,14 +11,19 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :nom, :email
+  attr_accessible :nom, :email, :password, :password_confirmation
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :nom,  :presence => true,
             :length   => { :maximum => 50 }
             :uniqueness => { :case_sensitive => false }
+
   validates :email, :presence => true,
             :format   => { :with => email_regex }
             :uniqueness => { :case_sensitive => false }
+
+  validates :password, :presence     => true,
+            :confirmation => true,
+            :length       => { :within => 6..40 }
 end
